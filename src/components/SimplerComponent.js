88b82b5f415415d9b001d4cf51641624e58614e3
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-03 15:56:30 -0400

    Completed assignment

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import React from  'react'
+import SimpleComponent from './SimpleComponent'
+
+function SimplerComponent(props) {
+    
+    return (<div onClick={props.handleClick} ><h1>I am just happy</h1></div>)
+}
+
+
+export default SimplerComponent
