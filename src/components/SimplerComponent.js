0151a64c46ffcc9aa72af19b6fdfb784a25c40cf
Ethan Rodriguez <ethan.rodriguez@mac.com>
2021-06-03 15:51:45 -0400

    commit tree be525da3eb3d2d3ff6a6071ff1ea33468cfa40e4

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,11 @@
 // Code SimplerComponent Here
+import React from  'react'
+import SimpleComponent from './SimpleComponent'
+
+function SimplerComponent(props) {
+    
+    return (<div onClick={props.handleClick} >I am just happy</div>)
+}
+
+
+export default SimplerComponent
